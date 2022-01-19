ec1a6d70cc678b74798cdec0183a0cea8b103dde
Victor <victor.sauvaget@epitech.eu>
2022-01-19 17:12:01 +0100

    mv SlackLoginRoute to service, add slack user token into db

--- server/src/routes/services/SlackServiceRoute.ts
+++ server/src/routes/services/SlackServiceRoute.ts
@@ -2,16 +2,19 @@ import Route from "../../Route";
 
 import axios from 'axios';
 import express = require('express');
+import randomstring = require('randomstring');
+import ServiceController, {TokenData} from "../../controllers/ServiceController";
+import {authorization} from "../../middlewares/AuthMiddleware";
 
 export default class SlackServiceRoute extends Route {
 
     constructor() {
         super();
-        this.router.get('/', this.post);
-        this.router.get('/code', this.code);
+        this.router.get('/callback', authorization, this.callback);
+        this.router.get('/', this.login);
     }
 
-    private code(req: express.Request, res: express.Response) {
+    private callback(req: express.Request, res: express.Response) {
         const code: string = req.query['code'] as string;
 
         const oauthUrl = "https://slack.com/api/oauth.access?"
@@ -24,18 +27,30 @@ export default class SlackServiceRoute extends Route {
                 "Accept": "application/json"
             }
         }).then((response) => {
-            let {error, error_description} = response.data;
+            let {error, error_description, access_token, refresh_token} = response.data;
             if (error)
                 return res.status(400).json({success: false, error: error_description});
-            console.log(response);
-            return res.status(200);
+            let token: TokenData = {
+                key: randomstring.generate(),
+                created_at: new Date(),
+                type: 'slack',
+                token: {
+                    access_token: access_token,
+                    refresh_token: refresh_token,
+                }
+            }
+            new ServiceController().registerUserToken(req['user']['uuid'], token, () => {
+                return res.status(200).json({success: true, token: token});
+            }, (err) => {
+                return res.status(400).json({success: false, error: err});
+            })
             }, (msg) => {
                 return res.status(400).json({success: false, error: msg});
             }
         );
     }
 
-    private post(req: express.Request, res: express.Response) {
+    private login(req: express.Request, res: express.Response) {
         return res.redirect("https://slack.com/oauth/authorize?"
         + "client_id=" + process.env.SLACK_CLIENT_ID + "&"
         + "scope=chat:write:bot&");
