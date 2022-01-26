c1c27567184deabcd72fa5f80a2caee9e6bb74d2
Victor <victor.sauvaget@epitech.eu>
2022-01-26 18:01:06 +0100

    Server services add doc, slack bot and services add doc

--- server/src/bots/SlackBot.ts
+++ server/src/bots/SlackBot.ts
@@ -3,11 +3,21 @@ const { WebClient } = require("@slack/web-api");
 export default class SlackBot {
     private client: any;
 
+    /**
+     * Constructor
+     *
+     * @param token
+     */
     public constructor(token: string) {
         this.client = new WebClient(token);
         console.log("Slack Bot is created !");
     }
 
+    /**
+     * Get client
+     *
+     * @returns client
+     */
     public getClient() {
         return this.client;
     }
--- server/src/routes/services/ServiceAuthRoute.ts
+++ server/src/routes/services/ServiceAuthRoute.ts
@@ -5,8 +5,19 @@ import randomstring = require('randomstring');
 type token = (data: TokenData) => void;
 type error = (error: string) => void;
 
-export default class ServiceRoute {
+export default class ServiceAuthRoute {
 
+    /**
+     * Execute post request for services auth
+     *
+     * @param url
+     * @param body
+     * @param header
+     * @param userUUID
+     * @param tokenType - cf Services in "./src/models/Services"
+     * @param success - return token
+     * @param errorFunc return error
+     */
     public postRequest(url: string, body: object, header: object, userUUID: string, tokenType: string, success: token, errorFunc: error): void {
         axios.post(url, body, header).then((response) => {
             let {error, access_token, refresh_token} = response.data;
@@ -22,6 +33,16 @@ export default class ServiceRoute {
         })
     }
 
+    /**
+     * Execute get request for services auth
+     *
+     * @param url
+     * @param params
+     * @param userUUID
+     * @param tokenType - cf Services in "./src/models/Services"
+     * @param success - return token
+     * @param errorFunc - return error
+     */
     public getRequest(url: string, params: object, userUUID: string, tokenType: string, success: token, errorFunc: error): void {
         axios.get(url, {params}).then((response) => {
             let {error, access_token, refresh_token} = response.data;
@@ -37,6 +58,16 @@ export default class ServiceRoute {
         })
     }
 
+    /**
+     * Generate token
+     *
+     * @param tokenType - cf Services in "./src/models/Services"
+     * @param access_token
+     * @param refresh_token
+     * @param userUUID
+     * @param success - return token
+     * @param errorFunc - return error
+     */
     private token(tokenType: string, access_token: string, refresh_token: string, userUUID: string, success: token, errorFunc: error) {
         let token: TokenData = {
             key: randomstring.generate(),
