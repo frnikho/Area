9c9690cc7170c3f8d744a511314004cd5b113a53
Justin Menard <justinmenard@orange.fr>
2022-01-26 16:54:34 +0100

    Add succes toast when disconnet

--- mobile/src/screens/HomeScreen.tsx
+++ mobile/src/screens/HomeScreen.tsx
@@ -1,25 +1,33 @@
-import { Button, Center, Icon, Text } from 'native-base';
-import React, { Component } from 'react';
+import {Button, Center, Toast, Text} from 'native-base';
+import React, {Component} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default class HomeScreen extends Component {
-    constructor(props: any) {
-        super(props);
-        this.onDisconnect = this.onDisconnect.bind(this);
-    }
+  constructor(props: any) {
+    super(props);
+    this.onDisconnect = this.onDisconnect.bind(this);
+  }
 
-    onDisconnect() {
-        AsyncStorage.removeItem("@token").then(() => {
-            this.props.navigation.navigate('login')
-        })
-    }
+  onDisconnect() {
+    AsyncStorage.removeItem('@token').then(() => {
+      Toast.show({
+        title: 'You are successfully disconnected',
+        status: 'success',
+        description: 'You can now login.',
+        duration: 2000,
+      });
+      this.props.navigation.navigate('login');
+    });
+  }
 
-    render() {
-        return (
-            <Center>
-                <Text>Home Screen</Text>
-                <Button colorScheme="error" onPress={this.onDisconnect} >Logout</Button>
-            </Center>
-        )
-    }
+  render() {
+    return (
+      <Center>
+        <Text>Home Screen</Text>
+        <Button colorScheme="error" onPress={this.onDisconnect}>
+          Logout
+        </Button>
+      </Center>
+    );
+  }
 }
