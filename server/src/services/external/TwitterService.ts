3bb9739e1d82f1620eacc9f934060ea78e4d711a
Victor <victor.sauvaget@epitech.eu>
2022-01-29 11:16:41 +0100

    Server Twitter Services add doc

--- server/src/services/external/TwitterService.ts
+++ server/src/services/external/TwitterService.ts
@@ -5,8 +5,15 @@ type error = (error: string) => void;
 
 export default class TwitterService {
 
+    /**
+     * Send Tweet
+     *
+     * @param userAccesToken - Given during OAuth
+     * @param message - Tweet message
+     * @param successFunc - success callback function
+     * @param errorFunc - error callback function
+     */
     public SendTweet(userAccesToken: string, message: string, successFunc: success, errorFunc: error) {
-
         const headers = {
             headers: {
                 "Content-type": "application/json",
@@ -19,8 +26,7 @@ export default class TwitterService {
         }
 
         axios.post("https://api.twitter.com/2/tweets", body, headers).then((response) => {
-            console.log(response);
-            return successFunc(response);
+            return successFunc("Tweet Sended");
         }).catch((err) => {
             return errorFunc(err);
         })
