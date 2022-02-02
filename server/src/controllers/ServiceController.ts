b38841003c1735f25d07a6e42c6f84bbec90d059
Victor <victor.sauvaget@epitech.eu>
2022-02-02 10:51:56 +0100

    server twitter refresh_token refactoring

--- server/src/controllers/ServiceController.ts
+++ server/src/controllers/ServiceController.ts
@@ -24,7 +24,6 @@ export default class ServiceController {
             if (result.length === 0)
                 return success(null);
             try {
-                console.log(result[0][service]);
                 return success(result[0][service]);
             } catch (ex) {
                 console.log('abc', ex);
@@ -57,16 +56,37 @@ export default class ServiceController {
         }, error);
     }
 
-    public getTokenByKeyAndService(userUuid: string, service: string, key: string, success: (token: TokenData | undefined) => void, error: error) {
-        if (service === undefined)
+    public getTokenByKeyAndService(userUuid: string, serviceName: string, key: string, success: (token: TokenData | undefined) => void, error: error) {
+        if (serviceName === undefined)
             return error("Invalid reaction service type !");
-        this.getTokensForService(userUuid, service, (tokens) => {
-            let obj = JSON.parse(tokens[0][service]);
+        this.getTokensForService(userUuid, serviceName, (tokens) => {
+            let obj = JSON.parse(tokens[0][serviceName]);
             let good = obj.filter((token) => token.key === key);
             if (good.length === 0)
                 return success(undefined);
             return success(good[0]);
         }, error);
+    }
+
+    /**
+     * Get token key by access token
+     *
+     * @param userUuid
+     * @param serviceName
+     * @param accessToken
+     * @param callback
+     * @returns
+     */
+    public getTokenByAccessTokenAndService(userUuid: string, serviceName: string, accessToken: string, callback: (status: boolean, response: any) => void) {
+        if (serviceName == undefined)
+            return callback(false, "Invalid reaction service type !");
+        DBService.query(`SELECT ${serviceName} FROM services WHERE user_uuid = '${userUuid}'`, (result) => {
+            let obj = JSON.parse(result[0][serviceName]);
+            let token = obj.filter((token) => token.token["access_token"] === accessToken);
+            if (token.length === 0)
+                return callback(false, "No token founded for user : " + userUuid);
+            return callback(true, token[0])
+        });
     }
 
     /**
@@ -80,7 +100,7 @@ export default class ServiceController {
     public updateTokenByKeyAndService(newToken: TokenData, userUuid: string, callback: (status: boolean, response: string) => void) {
         let serviceName = newToken.type;
         if (serviceName === undefined)
-        return callback(false, "Invalid reaction service type !");
+            return callback(false, "Invalid reaction service type !");
         let key = newToken.key;
         this.getTokensForService(userUuid, serviceName, (tokens) => {
             let obj = JSON.parse(tokens[0][serviceName]);
@@ -90,7 +110,7 @@ export default class ServiceController {
                     token.token["refresh_token"] = newToken.token["refresh_token"]
                 }
             });
-            DBService.query(`UPDATE area.services query SET query.${serviceName} = '${JSON.stringify(obj)}' WHERE query.user_uuid = '${userUuid}'`, (result) => {
+            DBService.query(`UPDATE area.services services SET services.${serviceName} = '${JSON.stringify(obj)}' WHERE services.user_uuid = '${userUuid}'`, (result) => {
                 return callback(true, "Tokens for user : " + userUuid + " has been updated");
             });
         }, (error) => {
--- server/src/services/external/TwitterService.ts
+++ server/src/services/external/TwitterService.ts
@@ -5,89 +5,104 @@ import {TokenData} from "../../controllers/ServiceController";
 
 const utf8 = require('utf8');
 
-type success = (data: any) => void;
-type error = (error: any) => void;
 
 export default class TwitterService {
 
     /**
      * Send Tweet
      *
-     * @param userAccessToken - Given during OAuth
-     * @param message - Tweet message
-     * @param userUuid - user uuid
+     * @param userAccesToken
+     * @param userUuid
+     * @param message
      * @param callback - status = true = success - status = false = error
      */
     public SendTweet(userAccesToken: string, userUuid: string, message: string, callback: (status: boolean, response: string) => void) {
-        const headers = {
-            headers: {
-                "Content-type": "application/json",
-                "Authorization": "Bearer " + userAccesToken
-            }
-        };
 
         let body = {
             text: message
         }
 
-        this.post("https://api.twitter.com/2/tweets", body, headers, (response) => {
-            return callback(true, response.data);
-        }, (err) => {
-            if (err.response.status !== undefined && err.response.status === 401) {
+        this.post("https://api.twitter.com/2/tweets", body,
+        {
+            headers: {
+                "Content-type": "application/json",
+                "Authorization": "Bearer " + userAccesToken
+            }
+        }, (status, response) => {
+            if (status) {
+                console.log("Tweet sended");
+                return callback(status, response.data);
+            }
+            if (response.response.status !== undefined && response.response.status === 401) {
                 console.log("access_token of " + userUuid + " users has been expired");
-                this.refreshToken(userUuid, (status, response) => {
-                    if (status)
+                this.refreshToken(userUuid, userAccesToken, (status, response) => {
+                    if (status) {
                         console.log("New access_token has been generated for user : " + userUuid);
-                    return callback(status, response);
+                        this.post("https://api.twitter.com/2/tweets", body,
+                        {
+                            headers: {
+                                "Content-type": "application/json",
+                                "Authorization": "Bearer " + response.new_access_token
+                            }
+                        }, (status, response) => {
+                            return callback(status, response);
+                        });
+                    } else {
+                        return callback(status, response);
+                    }
                 });
             }
         });
-
     }
 
     /**
-     * Refresh Twitter access token
+     * Refresh twitter token
      *
      * @param userUuid
+     * @param accessToken
      * @param callback - status = true = success - status = false = error
      */
-    private refreshToken(userUuid: string, callback: (status: boolean, response: string) => void) {
-        new ServiceController().getTokensForService(userUuid, Services.TWITTER.valueOf(), (response) => {
-            let string = utf8.encode(process.env.TWITTER_SERVICES_CLIENT_ID + ":" + process.env.TWITTER_SERVICES_CLIENT_SECRET);
+    private refreshToken(userUuid: string, accessToken: string, callback: (status: boolean, response: any) => void) {
+        // Get key by user's access token and service
+        new ServiceController().getTokenByAccessTokenAndService(userUuid, Services.TWITTER.valueOf(), accessToken, (status, response) => {
+            if (status) {
+                // Refresh token
+                let string = utf8.encode(process.env.TWITTER_SERVICES_CLIENT_ID + ":" + process.env.TWITTER_SERVICES_CLIENT_SECRET);
 
-            const headers = {
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    "Authorization": "Basic " + utf8.decode(Buffer.from(string).toString("base64"))
-                }
-            };
-
-            let array = JSON.parse(response[0][Services.TWITTER.valueOf()]);
-
-            const params = new URLSearchParams();
-            params.append('client_id', process.env.TWITTER_SERVICES_CLIENT_ID);
-            params.append("grant_type", "refresh_token");
-            params.append("refresh_token", array[0].token["refresh_token"]);
-
-
-            this.post("https://api.twitter.com/2/oauth2/token", params, headers, (response) => {
-                    let token: TokenData = {
-                        key: array[0].key,
-                        created_at: array[0].created_at,
-                        type: array[0].type,
-                        token: {
-                            access_token: response.data.access_token,
-                            refresh_token: response.data.refresh_token,
-                        }
+                const headers = {
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                        "Authorization": "Basic " + utf8.decode(Buffer.from(string).toString("base64"))
                     }
-                new ServiceController().updateTokenByKeyAndService(token, userUuid, (status, response) => {
-                    return callback(status, response);
+                };
+
+                const params = new URLSearchParams();
+                params.append('client_id', process.env.TWITTER_SERVICES_CLIENT_ID);
+                params.append("grant_type", "refresh_token");
+                params.append("refresh_token", response.token["refresh_token"]);
+
+                this.post("https://api.twitter.com/2/oauth2/token", params, headers, (status, postReponse) => {
+                    if (status) {
+                        let token: TokenData = {
+                            key: response.key,
+                            created_at: response.created_at,
+                            type: response.type,
+                            token: {
+                                access_token: postReponse.data.access_token,
+                                refresh_token: postReponse.data.refresh_token,
+                            }
+                        }
+                        // Update token by key and service in DB
+                        new ServiceController().updateTokenByKeyAndService(token, userUuid, (status, response) => {
+                            if (status)
+                                return callback(status, {msg: response, new_access_token: postReponse.data.access_token});
+                            return callback(status, response);
+                        });
+                    } else
+                        return callback(status, response)
                 });
-            }, (err) => {
-                return callback(false, err.data);
-            });
-        }, (err) => {
-            return callback(false, err);
+            } else
+                return callback(status, response);
         });
     }
 
@@ -97,14 +112,13 @@ export default class TwitterService {
      * @param url
      * @param body
      * @param headers
-     * @param successFunc
-     * @param errorFunc
+     * @param callback - status = true = success - status = false = error
      */
-    private post(url: string, body: object, headers: object, successFunc: success, errorFunc: error) {
+    private post(url: string, body: object, headers: object, callback: (status: boolean, response: any) => void) {
         axios.post(url, body, headers).then((response) => {
-            return successFunc(response);
+            return callback(true, response.data);
         }).catch((err) => {
-            return errorFunc(err);
+            return callback(false, err.data);
         });
     }
 }
