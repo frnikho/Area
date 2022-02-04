dcc8da68bb8be9dff362a403532b7e4639033952
Theo <theo.fargeas@epitech.eu>
2022-02-04 14:35:50 +0100

    Fix controllerHome

--- web/src/Controllers/ControllerHome.js
+++ web/src/Controllers/ControllerHome.js
@@ -1,38 +1,7 @@
-import React from "react";
-import {AuthContext} from "../Contexts/AuthContext";
-import HomePage from "../Views/HomePage";
-
 export default class ControllerHome {
 
-    static contextType = AuthContext;
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            redirectUrl: undefined,
-            user: undefined,
-        }
+    constructor(cookies, page) {
+        this.cookies = cookies;
+        this.page = page;
     }
-
-    componentWillMount() {
-        const auth = this.context;
-        if (auth.getUser() === undefined) {
-            this.setState({
-                redirectUrl: '/auth/login'
-            })
-        } else {
-            this.setState({
-                user: auth.getUser()
-            })
-            this.setState({
-                redirectUrl: '/area/dashboard'
-            })
-        }
-    }
-
-    render() {
-        return (<HomePage {...this}/>);
-    }
-
-
 }
