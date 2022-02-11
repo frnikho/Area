215aed1431ce890b7c76a096b3e9c7f473f7aabd
Theo <theo.fargeas@epitech.eu>
2022-02-11 13:57:06 +0100

    Move style

--- web/src/Components/SwitchButton.js
+++ web/src/Components/SwitchButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import "./SwitchButton.css"
+import "../Resources/Styles/SwitchButton.css"
 
 export default function SwitchButton({ isOn, ...props }) {
     const className = `switch ${isOn ? "on" : "off"}`;
