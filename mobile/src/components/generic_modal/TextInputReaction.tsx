0610b5ff08b2e7220777ac307015586f267bf76d
Justin Menard <justinmenard@orange.fr>
2022-02-25 09:38:47 +0100

    Create same text input modal for reaction

---
+++ mobile/src/components/generic_modal/TextInputReaction.tsx
@@ -0,0 +1,34 @@
+import {Box, Center, Input} from 'native-base';
+import React from 'react';
+import ReactionModal from '../ReactionModal';
+
+export default class TextInputAction extends ReactionModal {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      serviceName: this.props.serviceName,
+      text: undefined,
+    };
+    this.onChangeParam = this.onChangeParam.bind(this);
+  }
+
+  loadData() {}
+
+  onChangeParam(text: string) {
+    this.props.onChangeParam([text]), this.setState({text: text});
+  }
+
+  renderBody() {
+    return (
+      <Center>
+        <Box w="3/4" maxW="300">
+          <Input
+            mt="3"
+            placeholder={this.props.placeholder}
+            onChangeText={val => this.onChangeParam(val)}
+          />
+        </Box>
+      </Center>
+    );
+  }
+}
